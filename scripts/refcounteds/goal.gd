1715ef91627c5144e95b59db81ad1f0c875f3c90
CodeCorrupt <tylerhoyt93@gmail.com>
2025-09-28 19:09:45 -0400

    Add property to Goal that tracks it's last successful completion
    The completion time is tracked on the agent so it maintains unique
    per-agent value.

--- scripts/refcounteds/goal.gd
+++ scripts/refcounteds/goal.gd
@@ -2,6 +2,13 @@
 class_name Goal
 extends RefCounted
 
+var name := ""
+
+## Initialize the goal with a name.
+func _init(name: String = ""):
+	if name == "":
+		name = "Goal_%s" % str(self.get_instance_id())
+	self.name = name
 
 ## How rewarding is this goal to complete?  This can depend on dynamic factors (i.e. eating is
 ## more rewarding if an agent is hungry).
@@ -15,3 +22,21 @@ func compute_reward(agent: GdPAIAgent) -> float:
 ## Returns an array of preconditions.
 func get_desired_state(agent: GdPAIAgent) -> Array[Precondition]:
 	return []
+
+
+## Mark the goal as satisfied.
+func mark_goal_satisfied(agent: GdPAIAgent):
+	var last_time_goals_satisfied: Dictionary = agent.blackboard.get_property("GDPAI_GOALS_SATISFIED_TIMES", {})
+	if not last_time_goals_satisfied:
+		last_time_goals_satisfied = {}
+	last_time_goals_satisfied[name] = Time.get_ticks_msec()
+	agent.blackboard.set_property("GDPAI_GOALS_SATISFIED_TIMES", last_time_goals_satisfied)
+
+
+## Get the time since the goal was last satisfied.
+func time_since_satisfied(agent: GdPAIAgent) -> int:
+	var last_time_goals_satisfied: Dictionary = agent.blackboard.get_property("GDPAI_GOALS_SATISFIED_TIMES", {})
+	var current_time: int = Time.get_ticks_msec()
+	if not last_time_goals_satisfied.has(name):
+		return current_time - 0
+	return current_time - last_time_goals_satisfied[name]
